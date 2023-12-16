949a7542a7f68b69dccd52d2f0fb399631a20a5d
Dan Steeves <dan@thesteeves.org>
2023-12-16 17:22:55 -0600

    Day 15 Part 2

--- 15/foo.py
+++ 15/foo.py
@@ -15,3 +15,40 @@ def init(data: str) -> int:
     for step in data.split(","):
         total += hash(input=step)
     return total
+
+
+class Boxes:
+    def __init__(self):
+        self.boxes = [[] for a in range(0, 256)]
+
+    def process(self, instruction):
+        if instruction[-1] == "-":
+            focal_length = None
+            label = instruction[:-1]
+            box = hash(label)
+            self.boxes[box] = [a for a in self.boxes[box] if a[0] != label]
+        else:
+            focal_length = instruction[-1]
+            label = instruction[:-2]
+            box = hash(label)
+            if label in [a[0] for a in self.boxes[box]]:
+                index = [a[0] for a in self.boxes[box]].index(label)
+                self.boxes[box][index] = (label, focal_length)
+            else:
+                self.boxes[box].append((label, focal_length))
+        # print(instruction, box, label, focal_length, self.boxes[:4])
+
+    def score(self):
+        score = 0
+        for i, box in enumerate(self.boxes):
+            for j, lens in enumerate(box):
+                score += (i + 1) * (j + 1) * int(lens[1])
+                # print(i, j, lens[1], score)
+        return score
+
+
+def main(data):
+    box = Boxes()
+    for instruction in data.split(","):
+        box.process(instruction=instruction)
+    print(box.boxes)
--- 15/tests.py
+++ 15/tests.py
@@ -17,3 +17,26 @@ class TestAll(unittest.TestCase):
         with open("data.txt", "r") as f:
             data = f.read().strip()
         self.assertEqual(foo.init(data=data), 509784)
+
+    def test_main(self):
+        b = foo.Boxes()
+        for i in td1.split(","):
+            b.process(i)
+        self.assertEqual(
+            b.boxes[:4],
+            [
+                [("rn", "1"), ("cm", "2")],
+                [],
+                [],
+                [("ot", "7"), ("ab", "5"), ("pc", "6")],
+            ],
+        )
+        self.assertEqual(b.score(), 145)
+
+    def test_part_2(self):
+        b = foo.Boxes()
+        with open("data.txt", "r") as f:
+            data = f.read().strip()
+        for i in data.split(","):
+            b.process(i)
+        self.assertEqual(b.score(), 230197)
